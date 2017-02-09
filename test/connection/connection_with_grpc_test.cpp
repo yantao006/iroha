2f1b456579fe9afe991f8dc625936fba2980dbdd
mizuki.sonoko <mizuki.sonoko@gmail.com>
2017-02-09 19:46:33 +0900

    WIP Add test

--- test/connection/connection_with_grpc_test.cpp
+++ test/connection/connection_with_grpc_test.cpp
@@ -14,39 +14,63 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
 #include <string>
 #include <iostream>
+#include <thread>
 
 #include <unordered_map>
 
 #include "../../core/consensus/connection/connection.hpp"
 #include "../../core/consensus/consensus_event.hpp"
 #include "../../core/model/commands/transfer.hpp"
+#include "../../core/model/commands/add.hpp"
 #include "../../core/model/objects/domain.hpp"
 #include "../../core/model/transaction.hpp"
 #include "../../core/service/peer_service.hpp"
 
-#include "../../core/infra/protobuf/convertor.hpp"
-#include "../../core/infra/protobuf/event.grpc.pb.h"
-
 #include "../../core/infra/config/peer_service_with_json.hpp"
 
-template<typename T>
-using Transaction = transaction::Transaction<T>;
-template<typename T>
-using ConsensusEvent = event::ConsensusEvent<T>;
-template<typename T>
-using Add = command::Add<T>;
-template<typename T>
-using Transfer = command::Transfer<T>;
 
 int main(int argc, char* argv[]){
-    if(argc != 3){
-        return 1;
-    }
     logger::setLogLevel(logger::LogLevel::DEBUG);
-    connection::initialize_peer();
 
+    std::thread receiver_thread([](){
+      connection::initialize_peer();
+      connection::run();
+    });
+
+    connection::addSubscriber(
+      config::PeerServiceConfig::getInstance().getMyIp()
+    );
+
+    connection::receive([](const std::string& from,event::ConsensusEvent& event) {
+        logger::info("connection") << "receive!";
+        logger::info("sumeragi") << "received message! sig:[" << event.numberOfValidSignatures() << "]";
+    });
+
+    std::string senderPublicKey = "senderPublicKey";
+    std::unordered_map<std::string, object::BaseObject> values;
+    values.insert(std::make_pair("asset1", object::BaseObject("value")));
+    object::Asset asset(
+       "domain",
+       "name",
+       values
+    );
+
+    auto add = command::Add(object::Object(asset));
+    event::ConsensusEvent event(transaction::Transaction(
+      senderPublicKey,
+      command::Command(add)
+    ));
+    connection::send( config::PeerServiceConfig::getInstance().getMyIp(), event);
+
+    usleep(3);
     connection::finish();
+
+    receiver_thread.join();
     return 0;
 }
