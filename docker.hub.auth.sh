7323312a8c1c85548bc62fc9e81ab8c5bcff4ab2
Bogdan Vaneev <warchantua@gmail.com>
2017-03-13 18:19:18 +0300

    Fixes

--- docker.hub.auth.sh
+++ docker.hub.auth.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-sudo apt-get install -yqq expect
-
-
-if [ -z "$1" ]; then 
-	echo "Supply first argument: docker hub tag"
-fi
-
 
 if [[ ! -e ~/.docker/config.json ]]; then
 	expect -c '\
@@ -15,10 +8,7 @@ if [[ ! -e ~/.docker/config.json ]]; then
 	send -- "$env(DOCKER_USER)\r"
 	expect "Password:*"
 	send -- "$env(DOCKER_PASS)\r"
-	expect "Email:*"
-	send -- "$env(DOCKER_EMAIL)\r"
 	expect "*WARNING:*Login Succeeded*"
 	expect eof' 1>/dev/null || exit 1
 fi
 
-docker push $1
